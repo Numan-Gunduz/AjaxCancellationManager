7c333d7ad60b81e06c8d0a1ed0e7957bfc973392
Numan-Gunduz <sanayiveteknoloji>
2024-12-06 15:44:11 +0300

    CancellationToken Ve CancellationTokenSource

--- UrunYonetim/Controllers/UrunlerController.cs
+++ UrunYonetim/Controllers/UrunlerController.cs
@@ -1,50 +1,4 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using UrunYonetim.Data;
-//using UrunYonetim.Models;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using Microsoft.EntityFrameworkCore;
-
-//public class UrunlerController : Controller
-//{
-//    private readonly ApplicationDbContext _context;
-
-//    public UrunlerController(ApplicationDbContext context)
-//    {
-//        _context = context;
-//    }
-
-//    public async Task<IActionResult> Index()
-//    {
-//        var urunler = await _context.Urunler.ToListAsync();
-//        return View(urunler);
-//    }
-
-//    public async Task<IActionResult> UrunListele(CancellationToken cancellationToken)
-//    {
-//        try
-//        {
-//            Console.WriteLine("UrunListele işlemi başladı.");
-//            for (int i = 0; i < 100; i++)
-//            {
-//                await Task.Delay(100, cancellationToken);
-//            }
-//            var urunler = await _context.Urunler.ToListAsync(cancellationToken);
-//            Console.WriteLine("UrunListele işlemi tamamlandı.");
-//            return Json(urunler);
-//        }
-//        catch (TaskCanceledException)
-//        {
-//            Console.WriteLine("UrunListele işlemi iptal edildi.");
-//            return Json(new { message = "İşlem iptal edildi." });
-//        }
-//    }
-
-
-
-
-//}
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using UrunYonetim.Data;
 using UrunYonetim.Models;
 using System.Threading;
@@ -54,7 +8,6 @@ using Microsoft.EntityFrameworkCore;
 public class UrunlerController : Controller
 {
     private readonly ApplicationDbContext _context;
-    private static CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
 
     public UrunlerController(ApplicationDbContext context)
     {
@@ -67,36 +20,90 @@ public class UrunlerController : Controller
         return View(urunler);
     }
 
-    [HttpPost]
-    public async Task<IActionResult> UrunListele()
+    public async Task<IActionResult> UrunListele(CancellationToken cancellationToken)
     {
-        var cancellationToken = _cancellationTokenSource.Token;
         try
         {
-            Console.WriteLine("UrunListele işlemi başladı."); 
+            Console.WriteLine("UrunListele işlemi başladı.");
             for (int i = 0; i < 100; i++)
             {
                 await Task.Delay(100, cancellationToken);
             }
-
             var urunler = await _context.Urunler.ToListAsync(cancellationToken);
-            Console.WriteLine("UrunListele işlemi tamamlandı."); 
+            Console.WriteLine("UrunListele işlemi tamamlandı.");
             return Json(urunler);
         }
         catch (TaskCanceledException)
         {
-            Console.WriteLine("UrunListele işlemi iptal edildi."); 
+            Console.WriteLine("UrunListele işlemi iptal edildi.");
             return Json(new { message = "İşlem iptal edildi." });
         }
-
-    }
-
- 
-    [HttpPost]
-    public IActionResult CancelRequest()
-    {
-        _cancellationTokenSource.Cancel();  
-        _cancellationTokenSource = new CancellationTokenSource(); 
-        return Ok();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+//using Microsoft.AspNetCore.Mvc;
+//using UrunYonetim.Data;
+//using UrunYonetim.Models;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+
+//public class UrunlerController : Controller
+//{
+//    private readonly ApplicationDbContext _context;
+//    private static CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+//    public UrunlerController(ApplicationDbContext context)
+//    {
+//        _context = context;
+//    }
+
+//    public async Task<IActionResult> Index()
+//    {
+//        var urunler = await _context.Urunler.ToListAsync();
+//        return View(urunler);
+//    }
+
+//    [HttpPost]
+//    public async Task<IActionResult> UrunListele()
+//    {
+//        var cancellationToken = _cancellationTokenSource.Token;
+//        try
+//        {
+//            Console.WriteLine("UrunListele işlemi başladı.");
+//            for (int i = 0; i < 100; i++)
+//            {
+//                await Task.Delay(100, cancellationToken);
+//            }
+
+//            var urunler = await _context.Urunler.ToListAsync(cancellationToken);
+//            Console.WriteLine("UrunListele işlemi tamamlandı.");
+//            return Json(urunler);
+//        }
+//        catch (TaskCanceledException)
+//        {
+//            Console.WriteLine("UrunListele işlemi iptal edildi.");
+//            return Json(new { message = "İşlem iptal edildi." });
+//        }
+
+//    }
+
+
+//    [HttpPost]
+//    public IActionResult CancelRequest()
+//    {
+//        _cancellationTokenSource.Cancel();
+//        _cancellationTokenSource = new CancellationTokenSource();
+//        return Ok();
+//    }
+//}
--- UrunYonetim/wwwroot/js/urunler.js
+++ UrunYonetim/wwwroot/js/urunler.js
@@ -1,20 +1,23 @@
 ﻿
-   
 $(document).ready(function () {
+    let ajaxRequest;
+    let cancelRequest = false;
+    let startTime;
+
     $("#urunleriListeleBtn").click(function () {
         cancelRequest = false;
         startTime = new Date();
 
-      
         $("#urunleriListeleBtn").prop("disabled", true);
         $("#iptalBtn").show();
         $("#statusMessage").text("Listeleme işlemi başladı...");
+
         ajaxRequest = $.ajax({
             url: '/Urunler/UrunListele',
             type: 'POST',
             beforeSend: function () {
                 if (cancelRequest) {
-                    return false;
+                    return false; 
                 }
             },
             success: function (data) {
@@ -29,8 +32,8 @@ $(document).ready(function () {
                     });
                 }
 
-                var endTime = new Date();
-                var duration = (endTime - startTime) / 1000;
+                let endTime = new Date();
+                let duration = (endTime - startTime) / 1000;
                 $("#statusMessage").text("Listeleme işlemi tamamlandı. Geçen süre: " + duration + " saniye.");
             },
             complete: function () {
@@ -40,12 +43,13 @@ $(document).ready(function () {
             error: function () {
                 if (cancelRequest) return;
                 $("#urunListesi").append("<p>Veriler alınırken hata oluştu.</p>");
-                var endTime = new Date();
-                var duration = (endTime - startTime) / 1000;
+                let endTime = new Date();
+                let duration = (endTime - startTime) / 1000;
                 $("#statusMessage").text("Veri alınırken hata oluştu. Geçen süre: " + duration + " saniye.");
             }
         });
     });
+
 
     $("#iptalBtn").click(function () {
         cancelRequest = true;
@@ -54,31 +58,115 @@ $(document).ready(function () {
             ajaxRequest.abort();
         }
 
-       $("#urunListesi").append("<p>İşlem iptal edildi.</p>");
-        var endTime = new Date();
-        var duration = (endTime - startTime) / 1000;
+        $("#urunListesi").append("<p>İşlem iptal edildi.</p>");
+        let endTime = new Date();
+        let duration = (endTime - startTime) / 1000;
         $("#statusMessage").text("İşlem iptal edildi. Geçen süre: " + duration + " saniye.");
         $("#urunleriListeleBtn").prop("disabled", false);
         $("#iptalBtn").hide();
-
-        $.ajax({
-            url: '/Urunler/CancelRequest',
-            type: 'POST'
-        });
     });
-  
+
     $(window).on('beforeunload', function () {
         cancelRequest = true;
 
         if (ajaxRequest) {
-            ajaxRequest.abort();
+            ajaxRequest.abort(); 
         }
-
-        $.ajax({
-            url: '/Urunler/CancelRequest',
-            type: 'POST'
-        });
-
-        $("#statusMessage").text("Sayfadan ayrılma sırasında işlem iptal edildi.");
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+//$(document).ready(function () {
+//    $("#urunleriListeleBtn").click(function () {
+//        cancelRequest = false;
+//        startTime = new Date();
+
+      
+//        $("#urunleriListeleBtn").prop("disabled", true);
+//        $("#iptalBtn").show();
+//        $("#statusMessage").text("Listeleme işlemi başladı...");
+//        ajaxRequest = $.ajax({
+//            url: '/Urunler/UrunListele',
+//            type: 'POST',
+//            beforeSend: function () {
+//                if (cancelRequest) {
+//                    return false;
+//                }
+//            },
+//            success: function (data) {
+//                if (cancelRequest) return;
+
+//                $("#urunListesi").empty();
+//                if (data.message) {
+//                    $("#urunListesi").append("<p>" + data.message + "</p>");
+//                } else {
+//                    data.forEach(function (urun) {
+//                        $("#urunListesi").append("<p>" + urun.ad + " - " + urun.fiyat + " TL</p>");
+//                    });
+//                }
+
+//                var endTime = new Date();
+//                var duration = (endTime - startTime) / 1000;
+//                $("#statusMessage").text("Listeleme işlemi tamamlandı. Geçen süre: " + duration + " saniye.");
+//            },
+//            complete: function () {
+//                $("#urunleriListeleBtn").prop("disabled", false);
+//                $("#iptalBtn").hide();
+//            },
+//            error: function () {
+//                if (cancelRequest) return;
+//                $("#urunListesi").append("<p>Veriler alınırken hata oluştu.</p>");
+//                var endTime = new Date();
+//                var duration = (endTime - startTime) / 1000;
+//                $("#statusMessage").text("Veri alınırken hata oluştu. Geçen süre: " + duration + " saniye.");
+//            }
+//        });
+//    });
+
+//    $("#iptalBtn").click(function () {
+//        cancelRequest = true;
+
+//        if (ajaxRequest) {
+//            ajaxRequest.abort();
+//        }
+
+//       $("#urunListesi").append("<p>İşlem iptal edildi.</p>");
+//        var endTime = new Date();
+//        var duration = (endTime - startTime) / 1000;
+//        $("#statusMessage").text("İşlem iptal edildi. Geçen süre: " + duration + " saniye.");
+//        $("#urunleriListeleBtn").prop("disabled", false);
+//        $("#iptalBtn").hide();
+
+//        $.ajax({
+//            url: '/Urunler/CancelRequest',
+//            type: 'POST'
+//        });
+//    });
+  
+//    $(window).on('beforeunload', function () {
+//        cancelRequest = true;
+
+//        if (ajaxRequest) {
+//            ajaxRequest.abort();
+//        }
+
+//        $.ajax({
+//            url: '/Urunler/CancelRequest',
+//            type: 'POST'
+//        });
+
+//        $("#statusMessage").text("Sayfadan ayrılma sırasında işlem iptal edildi.");
+//    });
+//});
